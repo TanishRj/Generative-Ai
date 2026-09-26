ec7317a747ec03c0a28d234aa2a0ee8d1ba3b7c7
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-26 15:44:24 +0530

    Update 1. Objects.py

--- 1. Complete Python/1. Data Types/1. Objects.py
+++ 1. Complete Python/1. Data Types/1. Objects.py
@@ -6,10 +6,21 @@ amount = 24
 # print(f"New Initial Amount : {amount}")
 
 
-# Immutable because they are changing reference of amount variable from 2 to 12
+# Immutable because they are changing reference of amount variable in memory from 2 to 12
 # Printing ID of both 12 and 24 for checking reference change
 # print(f"Id of 2: {id(12)}")
 # print(f"Id of 12: {id(24)}")
 
 # MUTABLE using set function from python
-mutable_variable = set()
+jug = set()
+print(f"Initial jug id : {id(jug)}")
+# Adding values using add
+jug.add("Water")
+jug.add("Salt")
+jug.add("Lemon")
+
+# Printing Values
+print(f"Initial jug id : {jug}")
+
+# Printing formatted id after adding values
+print(f"Initial jug id : {id(jug)}")
