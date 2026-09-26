3afda704eadbcc6651830d070cdefee641b44621
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-26 15:36:00 +0530

    Update 1. Objects.py

--- 1. Complete Python/1. Data Types/1. Objects.py
+++ 1. Complete Python/1. Data Types/1. Objects.py
@@ -1,5 +1,12 @@
+# IMMMUTABLE
 amount = 12
 print(f"Initial Amount : {amount}")
 
 amount = 24
 print(f"New Initial Amount : {amount}")
+
+
+# Immutable because they are changing reference of amount variable from 2 to 12
+# Printing ID of both 12 and 24 for checking reference change
+print(f"Id of 2: {id(12)}")
+print(f"Id of 12: {id(24)}")
